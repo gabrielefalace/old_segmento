541a79e9bb44c027582596288e9b85d5384a8cb9
Gabriele Falace <gabriele.falace@vivaneo.de>
2021-04-18 23:57:56 +0200

    fix: adjusted wrong parenthesis

--- project.clj
+++ project.clj
@@ -1,5 +1,5 @@
 (defproject io.github.gabrielefalace/segmento "1.0.0"
   :description "A general-purpose interval manipulation library for the JVM."
-  :url "http://example.com/FIXME" }
+  :url "https://github.com/gabrielefalace/segmento"
   :dependencies [[org.clojure/clojure "1.10.1"]]
   :repl-options {:init-ns segmento.core})
